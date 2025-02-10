2c8fecfd540be353e96e85b73a04895cdb0ed6f1
Flacaro <flavianacaroselli@gmail.com>
2025-02-10 13:00:28 +0100

    prove hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,12 @@ import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        sessionFactory.openSession();
+        System.out.println("Inizializzazione di Hibernate...");
+        HibernateUtil.getSessionFactory();
+        System.out.println("Hibernate inizializzato con successo!");
+
+        // Puoi decidere di terminare qui, oppure fare altre operazioni
+        HibernateUtil.shutdown();
 
     }
 }

f28b60c329e8a863253d50cf3cf9be0cf1c699e7
Flacaro <flavianacaroselli@gmail.com>
2025-02-20 16:07:35 +0100

    problem adjacentArea

--- src/main/java/persistence/AreaDaoImpl.java
+++ src/main/java/persistence/AreaDaoImpl.java
@@ -1,9 +1,14 @@
 package persistence;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import model.Area;
+import model.CraftingCatalog;
+import model.Map;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AreaDaoImpl implements AreaDao {
 
@@ -20,6 +25,27 @@ public class AreaDaoImpl implements AreaDao {
             e.printStackTrace();
             em.getTransaction().rollback();
         }
+    }
+
+    public ArrayList<Area> getTotalMapArea(Long mapId, EntityManager em) {
+
+        ArrayList<Area> aree = new ArrayList<>();
+
+        try {
+            TypedQuery<Map> query = em.createQuery("SELECT m FROM Map m where id = :id", Map.class);
+            query.setParameter("id", mapId);
+
+            List<Map> maps = query.getResultList();
+            if(!maps.isEmpty()) {
+                Map map = maps.get(0);
+                aree.addAll(map.getTotalMapArea());
+            }
+            return aree;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        return null;
     }
 
 }
